72957d530782380532a434862b725aeb044efc9c
Nilsu Öz <nilsu-oz@hotmail.com>
2021-04-23 01:56:42 +0300

    update camera position

--- lib/components/map/mapBox.dart
+++ lib/components/map/mapBox.dart
@@ -1,8 +1,13 @@
+import 'package:ayarla/constants/constants.dart';
+import 'package:ayarla/constants/router.dart';
+import 'package:ayarla/models/functions.dart';
 import 'package:ayarla/virtual_data_base/businessOrUser_data.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import 'package:mapbox_search_flutter/mapbox_search_flutter.dart';
 import 'package:provider/provider.dart';
+
+import '../circularParent.dart';
 class MapBox extends StatefulWidget {
   static const id = 'MapBoxSample';
 
@@ -11,64 +16,96 @@ class MapBox extends StatefulWidget {
 }
 
 class _MapBoxState extends State<MapBox> {
+
+  Functions functions = Functions();
   @override
   Widget build(BuildContext context) {
-    void _showDialog() {
-      // flutter defined function
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          // return object of type Dialog
-          return AlertDialog(
-            title: Text("Adresinizi giriniz"),
-            content:  MapBoxPlaceSearchWidget(
-              popOnSelect: true,
-              apiKey: "pk.eyJ1Ijoibmlsc3VveiIsImEiOiJja25jOTV6cmExanZrMnhxbmNxdm9nMWZvIn0.FeH5rtpx5yIc3b-0To-XJg",
-              searchHint: 'Your Hint here',
-              onSelected: (place) {
-                Provider.of<BusinessAndUserData>(context, listen: false).setPickedPlace(place);
-              },
-              context: context,
-            ),
-            actions: <Widget>[
-              // usually buttons at the bottom of the dialog
-              TextButton(
-                child: Text("Adrese Git"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
+    MapboxMapController mapController;
+    LatLng coordinates = Provider.of<BusinessAndUserData>(context, listen: true).coiffurePosition;
+    void _onMapCreated(MapboxMapController controller) {
+      mapController = controller;
     }
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        flexibleSpace: CircularParent(
+          radius: 20,
+          gradient: functions.decideColor(context),
+          direction: Directions.bottom,
+        ),
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 10),
+          icon: BackButton(),
+          onPressed: () {
+            Routers.router.pop(context);
+          },
+        ),
+        title: Center(
+            child: Text(
+              "ayarla",
+              style: kTitleStyle.copyWith(
+                  color: Colors.white, letterSpacing: 3, fontSize: 25),
+            )),
+        actions: [
+          IconButton(
+            padding: EdgeInsets.only(right: 30, left: 10),
+            onPressed: () {
+            },
+            icon: Icon(Icons.account_circle, color: Colors.white, size: 35),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          _showDialog();
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              // return object of type Dialog
+              return AlertDialog(
+                title: Text("Adresinizi giriniz"),
+                content:  MapBoxPlaceSearchWidget(
+                  popOnSelect: false,
+                  apiKey: "pk.eyJ1Ijoibmlsc3VveiIsImEiOiJja25jOTV6cmExanZrMnhxbmNxdm9nMWZvIn0.FeH5rtpx5yIc3b-0To-XJg",
+                  searchHint: 'Your Hint here',
+                  onSelected: (place) {
+                    Provider.of<BusinessAndUserData>(context, listen: false).setPickedPlace(place);
+                    setState(()  {
+                      coordinates= Provider.of<BusinessAndUserData>(context, listen: false).coiffurePosition;
+                    });
+                  },
+                  context: context,
+                ),
+                actions: <Widget>[
+                  // usually buttons at the bottom of the dialog
+                  TextButton(
+                    child: Text("Adrese Git"),
+                    onPressed: () {
+                      mapController.moveCamera(CameraUpdate.newCameraPosition(
+                          CameraPosition(
+                            target: coordinates,
+                            zoom: 15,
+                          )
+                      ));
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
         child: Text("Ara"),
       ),
-      body: MapBoxWidget(coordinates: LatLng(Provider.of<BusinessAndUserData>(context, listen: true).selectedPlace.geometry.coordinates[0],
-          Provider.of<BusinessAndUserData>(context, listen: true).selectedPlace.geometry.coordinates[1]),),
-    );
-  }
-}
-
-class MapBoxWidget extends StatelessWidget {
-  final LatLng coordinates;
-  MapBoxWidget({this.coordinates});
-
-  @override
-  Widget build(BuildContext context) {
-    return MapboxMap(
-      styleString: "mapbox://styles/nilsuoz/ckncausp71d9z17r1wpcxss8p",
-      initialCameraPosition: CameraPosition(
-        zoom: 5.0,
-        target: coordinates ?? LatLng(41.015137, 28.979530),
+      body: MapboxMap(
+        accessToken: "pk.eyJ1Ijoibmlsc3VveiIsImEiOiJja25jOTV6cmExanZrMnhxbmNxdm9nMWZvIn0.FeH5rtpx5yIc3b-0To-XJg",
+        styleString: "mapbox://styles/nilsuoz/ckncausp71d9z17r1wpcxss8p",
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          zoom: 15.0,
+          target: coordinates,
+        ),
       ),
-
     );
   }
 }
--- lib/screens/coiffure_detail_page/coiffure_detail_page.dart
+++ lib/screens/coiffure_detail_page/coiffure_detail_page.dart
@@ -201,7 +201,6 @@ class _CoiffureDetailPageState extends State<CoiffureDetailPage> {
                                 .servicesAndEmployees);
                       },
                     );
-
                   },
                 ),
               ],
